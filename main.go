baba9e38f168801275b2d4ef7c9ee15f82975eac
Andrew <kolbe1129@gmail.com>
2021-09-30 18:15:08 -0500

    added a goodbye handler

--- main.go
+++ main.go
@@ -11,9 +11,12 @@ import (
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
 
 	// ListenAndServe contructs a http server and registers a default handler to it. If a handler is not specified, it uses DefaultServeMux
 	http.ListenAndServe(":9090", sm)
