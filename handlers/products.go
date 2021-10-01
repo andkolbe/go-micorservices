dfe7c4683d10f23df8a5d92641fde2569ae50e5c
Andrew <kolbe1129@gmail.com>
2021-10-01 00:17:38 -0500

    able to POST data

--- handlers/products.go
+++ handlers/products.go
@@ -23,18 +23,6 @@ func NewProducts(l *log.Logger) *Products {
 // we don't have to allocate memory for the data object. If you have a large json document, then that could be a real consideration 
 // the encoder is also faster. Makes a big deal with microservices
 
-// getProducts returns the products from the data store
-// reading JSON from our server
-func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
-	// fetch the products from the datastore
-	lp := data.GetProducts()
-	// convert the list of products to JSON to send back to the user
-	err := lp.ToJSON(rw)
-	if err != nil {
-		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
-	}
-}
-
 // ServeHTTP is the main entry point for the handler and satisfies the http.Handler interface
 // writing JSON back to the user
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
@@ -45,8 +33,41 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// handle an update
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
+
 
 	// catch all
 	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+// getProducts returns the products from the data store
+// reading JSON from our server
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET Products")
+
+	// fetch the products from the datastore
+	lp := data.GetProducts()
+	// convert the list of products to JSON to send back to the user
+	err := lp.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Product")
+
+	// create new product object
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		// we got a bad request
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest) 
+	}
+
+	p.l.Printf("Prod: %#v", prod)
+	data.AddProduct(prod)
 }
