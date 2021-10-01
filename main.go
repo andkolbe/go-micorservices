580aa86d9da648e93c96b2ff2e30e11f55d8e7ca
Andrew <kolbe1129@gmail.com>
2021-09-30 19:01:11 -0500

    created product list

--- main.go
+++ main.go
@@ -13,9 +13,12 @@ import (
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+
+	// create the handlers
 	hh := handlers.NewHello(l)
 	gh := handlers.NewGoodbye(l)
 
+	// create a new server mux and register the handlers
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
