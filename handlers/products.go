5b4f84d5692f00112179cc7960775aa83cec3613
Andrew <kolbe1129@gmail.com>
2021-10-01 11:55:36 -0500

    updated PUT method with gorilla mux

--- handlers/products.go
+++ handlers/products.go
@@ -3,8 +3,10 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/andkolbe/go-microservices/data"
+	"github.com/gorilla/mux"
 )
 
 // Products is a http.Handler
@@ -17,12 +19,10 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
-
 // an encoder does the same thing as marshal but instead of returning a slice of data or an error, it's writing the output direct to an io writer
 // the reason we want to use an encoder and write direct is because then we aren't having to buffer anything into memory
-// we don't have to allocate memory for the data object. If you have a large json document, then that could be a real consideration 
+// we don't have to allocate memory for the data object. If you have a large json document, then that could be a real consideration
 // the encoder is also faster. Makes a big deal with microservices
-
 
 // getProducts returns the products from the data store
 // reading JSON from our server
@@ -38,7 +38,7 @@ func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+func (p *Products) AddProduct(rw http.ResponseWriter, r *http.Request) {
 	p.l.Println("Handle POST Product")
 
 	// create new product object
@@ -46,22 +46,28 @@ func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
 	err := prod.FromJSON(r.Body)
 	if err != nil {
 		// we got a bad request
-		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest) 
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
 	}
 
 	data.AddProduct(prod)
 }
 
-func (p *Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
-	p.l.Println("Handle PUT Product")
+func (p *Products) UpdateProducts(rw http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(rw, "Unable to convert id", http.StatusBadRequest)
+	}
+
+	p.l.Println("Handle PUT Product", id)
 
 	// create new product object
 	prod := &data.Product{}
 
-	err := prod.FromJSON(r.Body)
+	err = prod.FromJSON(r.Body)
 	if err != nil {
 		// we got a bad request
-		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest) 
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
 	}
 
 	err = data.UpdateProduct(id, prod)
