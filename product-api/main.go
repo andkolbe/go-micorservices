cae0da2741cf5a7d24da77ec9d91dc0b0c85ffc3
Andrew <kolbe1129@gmail.com>
2021-10-01 17:40:49 -0500

    added CORS

--- product-api/main.go
+++ product-api/main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/go-openapi/runtime/middleware"
 
 	"github.com/andkolbe/go-microservices/product-api/handlers"
+	gohandlers "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -50,10 +51,13 @@ func main() {
 	getR.Handle("/docs", sh)
 	getR.Handle("/swagger.yaml", http.FileServer(http.Dir("./")))
 
+	// CORS
+	ch := gohandlers.CORS(gohandlers.AllowedOrigins([]string{"*"}))
+
 	// create a new server
 	s := http.Server{
-		Addr:         ":9090",      // configure the bind address
-		Handler:      sm,                // set the default handler
+		Addr:         ":9090",           // configure the bind address
+		Handler:      ch(sm),                // set the default handler
 		ErrorLog:     l,                 // set the logger for the server
 		ReadTimeout:  5 * time.Second,   // max time to read request from the client
 		WriteTimeout: 10 * time.Second,  // max time to write response to the client
